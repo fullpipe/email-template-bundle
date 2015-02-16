0d538b0d1c87c88ecbd770e3e577ff2eaca5744f
Eugene Bravov <eugene.bravov@gmail.com>
2015-02-16 11:48:32 +0300

    cs fix

--- Factory/MessageFactoryInterface.php
+++ Factory/MessageFactoryInterface.php
@@ -6,8 +6,8 @@ interface MessageFactoryInterface
 {
     /**
      * Create \Swift_Message
-     * @param  string $templateName template name in fullpipe_email_template.templates
-     * @param  array  $context      template context
+     * @param  string         $templateName template name in fullpipe_email_template.templates
+     * @param  array          $context      template context
      * @return \Swift_Message
      */
     public function create($templateName, array $context);
